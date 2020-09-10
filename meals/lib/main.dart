512102b41728a15f57b7ac93daa34e229da663f9
Flavio Santana <fvvsantana@gmail.com>
2020-09-10 14:56:43 -0300

    159. Styling & Theming

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.pink,
           accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 299, 1),
+          //canvasColor: Color.fromRGBO(255, 254, 299, 1),
           fontFamily: 'Raleway',
           textTheme: ThemeData.light().textTheme.copyWith(
                 bodyText1: TextStyle(
@@ -25,8 +25,9 @@ class MyApp extends StatelessWidget {
                   color: Color.fromRGBO(20, 51, 51, 1),
                 ),
                 headline6: TextStyle(
-                  fontSize: 24,
+                  fontSize: 20,
                   fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
                 ),
               ),),
       home: CategoriesScreen(),
