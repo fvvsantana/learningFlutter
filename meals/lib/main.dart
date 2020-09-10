044396dd1c8f0b32f02f3ea6474e6cc1a2d86640
Flavio Santana <fvvsantana@gmail.com>
2020-09-10 13:21:21 -0300

    156. Starting With The App - Adding Base Data

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,9 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('DeliMeals'),
       ),
-      body: Center(
-        child: Text('Navigation time!'),
-      ),
+      body: CategoriesScreen(),
     );
   }
 }
