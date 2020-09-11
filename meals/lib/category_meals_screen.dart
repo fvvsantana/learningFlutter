df1637d38f17d7b4d689e332c4d150c77ec05ec5
Flavio Santana <fvvsantana@gmail.com>
2020-09-11 10:43:01 -0300

    165. Selecting Meals for a Chosen Category

--- meals/lib/category_meals_screen.dart
+++ meals/lib/category_meals_screen.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+import './models/meal.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static final routeName = '/category-meals';
 
   @override
   Widget build(BuildContext context) {
-    final arg = ModalRoute.of(context).settings.arguments as Map<String,String>;
-    final id = arg['id'];
-    final title = arg['title'];
+    final arg =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = arg['id'];
+    final categoryTitle = arg['title'];
+    final List<Meal> meals =
+        DUMMY_MEALS.where((meal) => meal.categories.contains(categoryId)).toList();
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(categoryTitle),
       ),
       body: Center(
-        child: Text(id),
-      ),
+          child: Column(
+        children: meals.map((meal) => Text(meal.title)).toList(),
+      )),
     );
   }
 }
