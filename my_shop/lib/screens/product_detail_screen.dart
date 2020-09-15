b9a6825205768dc8a115da68625b4d2b7e49eeec
Flavio Santana <fvvsantana@gmail.com>
2020-09-15 11:00:42 -0300

    195. Using Nested Models & Providers

--- my_shop/lib/screens/product_detail_screen.dart
+++ my_shop/lib/screens/product_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/products.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
--- my_shop/lib/widgets/products_grid.dart
+++ my_shop/lib/widgets/products_grid.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../models/products.dart';
+import '../providers/products.dart';
 import '../widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
@@ -13,11 +13,10 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-            products[i].id,
-            products[i].title,
-            products[i].imageUrl,
-          ),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (_) => products[i],
+        child: ProductItem(),
+      ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
