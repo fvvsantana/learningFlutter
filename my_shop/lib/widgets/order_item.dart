c4dbbe818c2263fafba889e82f82506b7d94c981
Flavio Santana <fvvsantana@gmail.com>
2020-09-16 16:02:23 -0300

    210. Making Orders Expandable & Stateful Widgets vs Providers

--- my_shop/lib/widgets/order_item.dart
+++ my_shop/lib/widgets/order_item.dart
@@ -1,29 +1,61 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
 
   @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_more : Icons.expand_less),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              height: min(180, widget.order.products.length * 20.0 + 10),
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              child: ListView(
+                children: widget.order.products
+                    .map((cartItem) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(cartItem.title),
+                            Text('${cartItem.quantity}x \$${cartItem.price}'),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
